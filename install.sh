53fd4b8451d19204e3ca1194711bbef9c77d4276
“7Ragnarok7” <anorak318@gmail.com>
2020-07-27 14:47:11 +0530

    Updated the readme and installation file

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
-#!/bin/bash -x
+#!/bin/bash
 
 echo .....................INSTALLING DEDMAP..........................
 sudo apt-get install python3 python3-pip
+read -p "Select python3 as default if it is not already from the menu below. Press anything to continue :- "
+sudo update-alternatives --config python
+sudo pip install -U pip
 sudo pip install -r requirements.txt
 sudo mkdir -p /usr/share/figlet
 sudo cp fonts/* /usr/lib/python3/dist-packages/pyfiglet/fonts/
