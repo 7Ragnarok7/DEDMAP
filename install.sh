4105ca3ced0166d9a4315f006a0586799c4ddcfc
Anurag Mondal <anorak318@gmail.com>
2020-07-28 14:20:50 +0530

    Fixed the installation file

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 echo .....................INSTALLING DEDMAP..........................
 sudo apt-get install python3 python3-pip
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 10
 read -p "Select python3 as default if it is not already from the menu below. Press anything to continue :- "
 sudo update-alternatives --config python
 sudo pip install -U pip
