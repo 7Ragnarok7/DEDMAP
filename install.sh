4fecb64986f9305c44e3cf80afa1dd3e54b5b4da
Anurag Mondal <anorak318@gmail.com>
2020-07-28 14:37:14 +0530

    Fixed the installation file

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ sudo apt-get install python3 python3-pip
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 10
 read -p "Select python3 as default if it is not already from the menu below. Press anything to continue :- "
 sudo update-alternatives --config python
-sudo pip install -U pip
-sudo pip install -r requirements.txt
+sudo pip3 install -U pip
+sudo pip3 install -r requirements.txt
 sudo mkdir -p /usr/share/figlet
 sudo cp fonts/* /usr/lib/python3/dist-packages/pyfiglet/fonts/
 sudo cp dedmap /usr/bin/
