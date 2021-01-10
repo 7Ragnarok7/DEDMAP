9310a8be938bc2c0c3529229e2787cfdfe05b23b
Anurag Mondal <anorak318@gmail.com>
2021-01-10 21:24:38 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,14 +2,15 @@
 
 echo -e "\n.....................INSTALLING DEDMAP..........................\n"
 chmod +x dedmap
-sudo apt-get install python3 python3-pip
-sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 10
+apt-get install python3 python3-pip
 read -p "Select python3 as default if it is not already from the menu below. Press anything to continue :- "
-sudo update-alternatives --config python
-sudo pip3 install -U pip
-sudo pip3 install -r requirements.txt
-sudo cp fonts/* /usr/lib/python3/dist-packages/pyfiglet/fonts/
-sudo cp fonts/* /usr/local/lib/python3/dist-packages/pyfiglet/fonts/
-sudo cp dedmap /usr/bin/
+update-alternatives --config python
+pip3 install -U pip
+pip3 install -r requirements.txt
+cp fonts/* /usr/lib/python3/dist-packages/pyfiglet/fonts/
+cp fonts/* /usr/local/lib/python3/dist-packages/pyfiglet/fonts/
+mkdir /opt/dedmap/
+cp dedmap /opt/dedmap/
+ln -s /opt/dedmap/dedmap /usr/bin/dedmap
 echo -e "\n......................INSTALLATION FINISHED......................"
 echo -e "\nThanks for installing dedmap.\nUse dedmap -h or --help for usage instructions.\n"
