0c755d193300cf0f99707af842061fa547f9695a
Anurag Mondal <anorak318@gmail.com>
2021-01-31 21:28:54 +0530

    Major Updates

--- install.sh
+++ install.sh
@@ -8,15 +8,16 @@ fi
 echo -e "\n.....................INSTALLING DEDMAP..........................\n"
 chmod +x dedmap
 apt-get update
-apt-get install python3 python3-pip figlet ping
+apt-get install python3 python3-pip figlet
+echo
 read -p "Select python3 as default if it is not already from the menu below. Press anything to continue :- "
 update-alternatives --config python
 pip3 install -U pip
 pip3 install -r requirements.txt
-cp fonts/* /usr/lib/python3/dist-packages/pyfiglet/fonts/
-cp fonts/* /usr/local/lib/python3/dist-packages/pyfiglet/fonts/
-mkdir /usr/share/dedmap/
-cp dedmap /usr/share/dedmap/
-ln -s /usr/share/dedmap/dedmap /usr/bin/dedmap
+cp -f fonts/* /usr/lib/python3/dist-packages/pyfiglet/fonts/
+cp -f fonts/* /usr/local/lib/python3/dist-packages/pyfiglet/fonts/
+mkdir -p /usr/share/dedmap/
+cp -f dedmap /usr/share/dedmap/
+ln -sf /usr/share/dedmap/dedmap /usr/bin/dedmap
 echo -e "\n......................INSTALLATION FINISHED......................"
 echo -e "\nThanks for installing dedmap.\nUse dedmap -h or --help for usage instructions.\n"
--- termux-install.sh
+++ termux-install.sh
@@ -2,14 +2,16 @@
 
 echo -e "\n.....................INSTALLING DEDMAP..........................\n"
 chmod +x dedmap
-apt-get install python3 python3-pip figlet ping
+apt-get update
+apt-get install python3 python3-pip figlet
+echo
 read -p "Select python3 as default if it is not already from the menu below. Press anything to continue :- "
 update-alternatives --config python
 pip3 install -U pip
 pip3 install -r requirements.txt
-cp fonts/* ~/../usr/lib/python3.9/site-packages/pyfiglet/fonts/
-mkdir ~/../usr/share/dedmap/
-cp dedmap ~/../usr/share/dedmap/
-ln -s ~/../usr/share/dedmap/dedmap ~/../usr/bin/dedmap
+cp -f fonts/* ~/../usr/lib/python3.9/site-packages/pyfiglet/fonts/
+mkdir -p ~/../usr/share/dedmap/
+cp -f dedmap ~/../usr/share/dedmap/
+ln -sf ~/../usr/share/dedmap/dedmap ~/../usr/bin/dedmap
 echo -e "\n......................INSTALLATION FINISHED......................"
 echo -e "\nThanks for installing dedmap.\nUse dedmap -h or --help for usage instructions.\n"
