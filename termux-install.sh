3809f5cb4c0de4cd667993deb10e5ed2b563e1ad
Anurag Mondal <anorak318@gmail.com>
2021-01-31 17:11:51 +0530

    Major Updates

--- termux-install.sh
+++ termux-install.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-if [ $(whoami) != 'root' ]; then
-	echo "Please run this script using sudo."
-	exit
-fi
-
 echo -e "\n.....................INSTALLING DEDMAP..........................\n"
 chmod +x dedmap
-apt-get update
 apt-get install python3 python3-pip figlet ping
 read -p "Select python3 as default if it is not already from the menu below. Press anything to continue :- "
 update-alternatives --config python
 pip3 install -U pip
 pip3 install -r requirements.txt
-cp fonts/* /usr/lib/python3/dist-packages/pyfiglet/fonts/
-cp fonts/* /usr/local/lib/python3/dist-packages/pyfiglet/fonts/
-mkdir /usr/share/dedmap/
-cp dedmap /usr/share/dedmap/
-ln -s /usr/share/dedmap/dedmap /usr/bin/dedmap
+cp fonts/* ~/../usr/lib/python3.9/site-packages/pyfiglet/fonts/
+mkdir ~/../usr/share/dedmap/
+cp dedmap ~/../usr/share/dedmap/
+ln -s ~/../usr/share/dedmap/dedmap ~/../usr/bin/dedmap
 echo -e "\n......................INSTALLATION FINISHED......................"
 echo -e "\nThanks for installing dedmap.\nUse dedmap -h or --help for usage instructions.\n"
