ff3b3fe86407d717abb090f6475d904143435edd
Anurag Mondal <anorak318@gmail.com>
2021-09-19 13:10:23 +0530

    Update termux-install.sh

--- termux-install.sh
+++ termux-install.sh
@@ -4,9 +4,6 @@ echo -e "\n.....................INSTALLING DEDMAP..........................\n"
 chmod +x dedmap
 apt-get update
 apt-get install python3 python3-pip figlet
-echo
-read -p "Select python3 as default if it is not already from the menu below. Press anything to continue :- "
-update-alternatives --config python
 pip3 install -U pip
 pip3 install -r requirements.txt
 pyfiglet -L fonts/Bloody.flf
