a04d5ff52c7a13b1b3f1cae4ed2f33c069898c12
Anurag Mondal <anorak318@gmail.com>
2022-04-10 19:33:16 +0530

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -8,4 +8,4 @@ fi
 rm /usr/bin/dedmap
 rm -rf /usr/share/dedmap/
 echo -e "\n.......................DEDMAP UNINSTALLED............................"
-echo -e "\nIt's sad to see you go :(\n"
+echo -e "\nIt's sad to see you leave :(\n"
